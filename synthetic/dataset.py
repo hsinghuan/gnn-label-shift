662a91f460d28984eea8c4eb62a79b70ea8db713
hsinghuan <hhchung@utexas.edu>
2023-09-28 09:11:05 -0500

    make the data loading function more flexible to user

--- synthetic/dataset.py
+++ synthetic/dataset.py
@@ -63,13 +63,13 @@ class StochasticBlockModelBlobDataset(InMemoryDataset):
         x = None
         if self.num_channels is not None:
 
-            x, y_not_sorted, c = make_blobs(n_samples=num_samples, n_features=self.num_channels, return_centers=True,
+            x, y_not_sorted, c = make_blobs(n_samples=self.block_sizes, n_features=self.num_channels, return_centers=True,
                                             random_state=self.random_state, **self.kwargs)
             x = x[np.argsort(y_not_sorted)]
             x = torch.from_numpy(x).to(torch.float)
 
         y = torch.arange(num_classes).repeat_interleave(self.block_sizes)
-        if self.flip_y >= 0.0:
+        if self.flip_y > 0.0:
             flip_mask = torch.bernoulli(self.flip_y * torch.ones_like(y)).type(torch.bool)
             y[flip_mask] = torch.randint(num_classes, size=(int(flip_mask.sum().item()),))
 
@@ -92,11 +92,10 @@ class StochasticBlockModelBlobDataset(InMemoryDataset):
         torch.save(self.collate([data]), self.processed_paths[0])
 
 
-def load_sbm_dataset(data_dir:str, src:bool, high_dim: bool):
-    subdir = "high_dim" if high_dim else "low_dim"
+def load_sbm_dataset(data_dir:str, src:bool):
     domain = "src" if src else "tgt"
-    dataset_name = "StochasticBlockModelDataset" if high_dim else "StochasticBlockModelBlobDataset"
-    leaf_dir = os.path.join(os.path.join(data_dir, subdir, domain, dataset_name, "processed"))
+    dataset_name = "StochasticBlockModelBlobDataset"
+    leaf_dir = os.path.join(os.path.join(data_dir, domain, dataset_name, "processed"))
     dir_list = os.listdir(leaf_dir)
     path = os.path.join(leaf_dir, list(filter(lambda fname: "data" in fname, dir_list))[0])
     data, _ = torch.load(path)
