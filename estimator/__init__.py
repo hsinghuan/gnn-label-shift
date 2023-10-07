682038fbe4f4d8a2237d012d7ab95764747e3073
hsinghuan <hhchung@utexas.edu>
2023-10-07 16:55:11 -0500

    finalize bbse to be as close as online implementation

---
+++ estimator/__init__.py
@@ -0,0 +1,1 @@
+from .bbse import BBSE
--- estimator/bbse.py
+++ estimator/bbse.py
@@ -1,19 +1,23 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
 
-import utils
 
 class BBSE():
     def __init__(self):
         pass
 
-    def estimate_importance_weight(self, y_true_src, y_pred_src, y_pred_tgt, n_classes):
+    def calculate_marginal(self, y, n_classes):
+        mu = np.zeros(shape=(n_classes, 1))
+        for i in range(n_classes):
+            mu[i] = np.sum(y == i)
+        return mu / y.shape[0]
 
+    def estimate_importance_weight(self, y_true_src, y_pred_src, y_pred_tgt, n_classes):
         labels = np.arange(n_classes)
         C = confusion_matrix(y_true_src, y_pred_src, labels=labels).T
         C = C / y_true_src.shape[0]
 
-        mu_t = utils.calculate_marginal(y_pred_tgt, n_classes)
+        mu_t = self.calculate_marginal(y_pred_tgt, n_classes)
         lamb = 1.0 / min(y_pred_src.shape[0], y_pred_tgt.shape[0])
 
         I = np.eye(n_classes)
@@ -21,7 +25,7 @@ class BBSE():
         return wt
 
     def estimate_target_dist(self, wt, y_true_src, n_classes):
-        mu_t = utils.calculate_marginal(y_true_src, n_classes)
+        mu_t = self.calculate_marginal(y_true_src, n_classes)
         return wt * mu_t
 
 
@@ -29,8 +33,6 @@ class BBSE():
 # y_pred_src = np.array([0, 0, 2, 2, 0, 2])
 # y_pred_tgt = np.array([0,1,1,1,2,2,1,1,1,1,1,2,2,0])
 # bbse = BBSE()
-# print(utils.calculate_marginal(y_true_src, n_classes=3))
-# print(utils.calculate_marginal(y_pred_tgt, n_classes=3))
 # wt = bbse.estimate_importance_weight(y_true_src, y_pred_src, y_pred_tgt, 3)
 # print(wt)
 # tgt_dist = bbse.estimate_target_dist(wt, y_true_src, 3)
